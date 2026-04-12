c5f9689eec94740a64023fab8376cb07c2810106
Bhavy <bhavy_mr@cs.iitr.ac.in>
2026-04-12 15:02:51 +0530

    Final Ordering

--- views/hostel_admin.py
+++ views/hostel_admin.py
@@ -12,7 +12,7 @@ from bhawan_app.models.resident import Resident
 from bhawan_app.serializers.hostel_admin import HostelAdminSerializer
 from bhawan_app.constants import designations
 from bhawan_app.managers.services import is_warden, is_global_admin, is_supervisor
-
+    
 Person = swapper.load_model('Kernel', 'Person')
 Hostel = swapper.load_model('Kernel', 'Residence')
 
